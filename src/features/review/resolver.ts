1ad12a14e590242693d2fae4445275f1f32357ae
Rémi de Juvigny <remidejuvigny@gmail.com>
2019-10-31 12:32:05 +0100

    Email notif of stats updates

--- src/features/review/resolver.ts
+++ src/features/review/resolver.ts
@@ -2,6 +2,7 @@ import { Resolver, FieldResolver, Root } from 'type-graphql'
 import { DocumentType } from '@typegoose/typegoose'
 import { Review, ReviewStats, ReviewStatsModel } from './model'
 import { Creator, CreatorModel } from '../creator/model'
+import { arrangeReviewStats } from '.'
 
 @Resolver(() => Review)
 class ReviewResolver {
@@ -13,10 +14,14 @@ class ReviewResolver {
 
   @FieldResolver({ description: 'Last stat objects, recent to old' })
   async stats(@Root() review: DocumentType<Review>): Promise<ReviewStats[]> {
+    // Get raw stats
     const stats = await ReviewStatsModel.find({ review: review._id })
       .sort({ createdAt: -1 })
       .limit(20)
-    return stats
+      .lean()
+    // Keep just one per day
+    const uniqueDayStats = arrangeReviewStats(stats)
+    return uniqueDayStats
   }
 }
 
--- src/utils/cron.ts
+++ src/utils/cron.ts
@@ -1,5 +1,6 @@
 import cron from 'node-cron'
 import { saveAllReviewsNewStats } from '../features/review'
+import { emailService } from './emails'
 
 export class CronTaskManager {
   constructor() {
@@ -11,8 +12,21 @@ export class CronTaskManager {
     // Every 10 seconds '*/5 * * * * *'
     // cron.schedule('*/5 * * * * *', async () => {
     cron.schedule('0 3 * * *', async () => {
-      const updatedCount = await saveAllReviewsNewStats()
-      console.log(`Updated stats for ${updatedCount} reviews`)
+      // Sequantially update all review stats
+      const { failedReviews, updatedCount } = await saveAllReviewsNewStats()
+      // Notify of the task success
+      console.log(`Updated stats for ${updatedCount} reviews, with ${failedReviews.length} errors`)
+      await emailService.send({
+        template: 'updateReviewStats',
+        locals: {
+          updatedCount,
+          failedReviews,
+        },
+        message: {
+          from: 'Revolt Gaming <campaigns@revoltgaming.co>',
+          to: process.env.DEVELOPER_EMAIL,
+        },
+      })
     })
   }
 }
